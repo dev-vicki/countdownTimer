73dbd485dd64d029db3aa222c9798cdd304173c7
dev-vicki <vignesh.r1999@gmail.com>
2022-01-10 12:53:00 +0530

    implemented reset and abort buttons with logic

--- app.js
+++ app.js
@@ -9,36 +9,79 @@
 
   let interval;
   let pause = false;
+  let totalSeconds = 0;
+  let totalSecondsBackup = 0;
   initiallyHideButtons();
 
-btnStart.addEventListener("click", ()=> {
-    const hour = parseInt(hoursElement.value);
-    const min = parseInt(minutesElement.value);
-    const sec = parseInt(secondsElement.value);
-    console.log(hour,min,sec);
 
-    const totalSeconds = hour * 60 * 60 + min * 60 + sec;
-     startTimer(totalSeconds);
-})
 
 function initiallyHideButtons(){
     btnPause.style.display = "none";
     btnStop.style.display = "none";
     btnReset.style.display = "none";
 
+    btnStart.addEventListener("click", () => {
+      const hour = parseInt(hoursElement.value);
+      const min = parseInt(minutesElement.value);
+      const sec = parseInt(secondsElement.value);
+      totalSecondsBackup = totalSeconds = hour * 60 * 60 + min * 60 + sec;
+
+      startTimer();
+
+      btnPause.style.display = "inline-block";
+      btnStop.style.display = "inline-block";
+      btnReset.style.display = "inline-block";
+      btnStart.style.display = "none";
+
+
+    });
+
+    btnPause.addEventListener("click", ()=>{
+        pause = !pause;
+        if(pause){
+            btnPause.innerText = "Resume";
+        } else {
+            btnPause.innerText = "Pause";
+        }
+    })
+
+    btnStop.addEventListener("click", ()=>{
+        stopTimer();
+        totalSeconds = totalSecondsBackup;
+        pause = false;
+        updateInputs();
+
+        btnPause.style.display = "none";
+        btnStop.style.display = "none";
+        btnReset.style.display = "none";
+        btnStart.style.display = "";
+    })
+
+    btnReset.addEventListener("click", ()=>{
+        totalSeconds = totalSecondsBackup;
+        updateInputs();
+    })
 }
 
-function startTimer(totalSeconds){
+function startTimer(){
     interval = setInterval(()=>{
+
+        if(pause) return;
+
         totalSeconds--;
-        updateInputs(totalSeconds);
+        updateInputs();
+
         if(totalSeconds<=0){
-          interval = clearInterval(interval);
+         stopTimer();
         }
     }, 1000)
 }
 
-function updateInputs(totalSeconds){
+function stopTimer(){
+     interval = clearInterval(interval);
+}
+
+function updateInputs(){
     const hours = Math.floor(totalSeconds / 60 / 60);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
